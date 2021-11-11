5bae7191cfd697485eb4c4373eea9b2ca3fd7868
Yunazaki <66821940+Yunazaki@users.noreply.github.com>
2021-11-10 22:22:42 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,11 @@ int main() {
 
 	cout << "Enter your Phone Number: " << endl;
 
-	cin >> PhoneNum[0] >> PhoneNum[1] >> PhoneNum[2] >> PhoneNum[3] >> PhoneNum[4] >> PhoneNum[5] >> PhoneNum[6] >> PhoneNum[7] >> PhoneNum[8] >> PhoneNum[9];
+	for (int i = 0; i < 10; i++) {
+
+		cin >> PhoneNum[i];
+
+	}
 
 	sort(PhoneNum, PhoneNum + 10);
 
@@ -22,7 +26,11 @@ int main() {
 
 	cout << "Enter some names: " << endl;
 
-	cin >> names[0] >> names[1] >> names[2] >> names[3] >> names[4] >> names[5] >> names[6] >> names[7] >> names[8] >> names[9] >> names[10] >> names[11] >> names[12] >> names[13] >> names[14] >> names[15] >> names[16] >> names[17] >> names[18] >> names[19];
+	for (int i = 0; i < 20; i++) {
+
+		cin >> names[i];
+
+	}
 
 	random_shuffle(names, names + 20);
 
